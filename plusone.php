25f6d09d8577456676005e40b242563826d5ec5e
WPMUDEV <WPMUDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-10 16:46:52 +0000

    1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/google/trunk@395628 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plusone.php
+++ plusone.php
@@ -3,7 +3,7 @@
 Plugin Name: Google +1
 Plugin URI: http://premium.wpmudev.org/project/google-1
 Description: Adds the Google +1 button to your site.
-Version: 1.0.2
+Version: 1.0.3
 Author: Ve Bailovity (Incsub)
 Author URI: http://premium.wpmudev.org
 WDP ID: 234
@@ -23,25 +23,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-
-///////////////////////////////////////////////////////////////////////////
-/* -------------------- Update Notifications Notice -------------------- */
-if ( !function_exists( 'wdp_un_check' ) ) {
-  add_action( 'admin_notices', 'wdp_un_check', 5 );
-  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-  function wdp_un_check() {
-    if ( class_exists( 'WPMUDEV_Update_Notifications' ) )
-      return;
-
-    if ( $delay = get_site_option( 'un_delay' ) ) {
-      if ( $delay <= time() && current_user_can( 'install_plugins' ) )
-      	echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
-	  } else {
-			update_site_option( 'un_delay', strtotime( "+1 week" ) );
-		}
-	}
-}
-/* --------------------------------------------------------------------- */
 
 define ('WDGPO_PLUGIN_SELF_DIRNAME', basename(dirname(__FILE__)), true);
 
